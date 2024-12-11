78819b6b3e44ba2f98df34564f5a86e3aafd27b5
Risa Yamamoto <yamari2422@gmail.com>
2024-12-11 11:30:38 -0800

    style: update style of the event card component

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -18,9 +18,9 @@ interface EventCardProps {
 
 const EventCard: React.FC<EventCardProps> = ({ event }) => {
   return (
-    <div className="flex flex-col sm:flex-row gap-3 mt-4 border w-full sm:w-2/3 md:w-1/2">
+    <div className="flex flex-col sm:flex-row mt-4 border w-full sm:w-2/3 lg:w-1/2 shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out rounded-lg">
       {/* Image Section */}
-      <div className="relative w-full sm:w-1/3 md:w-1/3 h-48 md:h-44 ">
+      <div className="relative w-full sm:w-1/3 md:w-1/3 h-48 md:h-44 overflow-hidden rounded-l-lg shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
         <Image
           src={event.image || "/image_sample.png"}
           alt="event image"
@@ -30,30 +30,37 @@ const EventCard: React.FC<EventCardProps> = ({ event }) => {
       </div>
 
       {/* Content Section */}
-      <div className="flex flex-col w-full sm:w-2/3 md:w-2/3 p-3 justify-between">
-        {event.price ? (
-          <dl className="mb-4">
-            <dd className="text-xl">
-              {(event.price.unit_amount / 100).toLocaleString()}{" "}
-              {event.price.currency.toUpperCase()}
-            </dd>
-          </dl>
-        ) : (
-          <h4 className="text-xl">Free</h4>
-        )}
-        <div className="flex flex-col">
-          <h2 className="text-lg font-semibold">{event.title["en-title"]}</h2>
-          <div className="flex flex-col sm:flex-row justify-between sm:items-end gap-3">
-            <div className="flex flex-col">
-              <p>{event.date["en-date"]}</p>
-              <p>{event.location["en-location"]}</p>
-            </div>
-            <div className="flex justify-end sm:ml-auto">
-              <Link href={`/events/${event.id}`}>
-                <Button variant="square" className="sm:w-auto w-[200px] lg:w-[150px]">
-                  View More
-                </Button>
-              </Link>
+      <div className="flex flex-col w-full gap-2 sm:w-2/3 md:w-2/3 p-5 md:py-4 px-3 ">
+        <div className="flex flex-col justify-between gap-5">
+          {event.price ? (
+            <dl className="w-[80px] bg-linkWater rounded-2xl">
+              <dd className="text-sm  text-gray-500 text-center font-semibold">
+                {(event.price.unit_amount / 100).toLocaleString()}{" "}
+                {event.price.currency.toUpperCase()}
+              </dd>
+            </dl>
+          ) : (
+            <h4 className="text-sm w-[80px] bg-linkWater text-center rounded-2xl text-gray-500 font-semibold">
+              Free
+            </h4>
+          )}
+          <div className="flex flex-col gap-2">
+            <h2 className=" text-lg md:text-base lg:text-lg font-semibold">{event.title["en-title"]}</h2>
+            <div className="flex flex-col sm:flex-row justify-between sm:items-end gap-3">
+              <div className="flex flex-col">
+                <p className="text-sm">{event.date["en-date"]}</p>
+                <p className="text-sm">{event.location["en-location"]}</p>
+              </div>
+              <div className="flex justify-end sm:ml-auto">
+                <Link href={`/events/${event.id}`}>
+                  <Button
+                    variant="square"
+                    className="sm:w-auto w-[200px] lg:w-[150px]"
+                  >
+                    View More
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
