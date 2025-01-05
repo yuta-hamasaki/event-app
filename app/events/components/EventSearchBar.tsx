2a9465b677e34ea4a2fc73789aced88b26f08af9
Risa Yamamoto <yamari2422@gmail.com>
2025-01-04 23:27:31 -0800

    feat: add base structure to the event page

---
+++ app/events/components/EventSearchBar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const EventSearchBar = (props: Props) => {
+  return <div>EventSearchBar</div>;
+};
+
+export default EventSearchBar;
--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { getEvents } from "@/lib/client";
+import EventCard from "@/components/EventCard";
+import EventSearchBar from "./components/EventSearchBar";
 
-const page = () => {
+const page = async () => {
+  const events = await getEvents();
+
   return (
-    <div>
-      イベント
-    </div>
-  )
-}
+    <section className="flex flex-col items-center">
+      <h1 className="text-4xl font-bold font-plus-jakarta-sans tracking-tight">
+        Events
+      </h1>
+      <div>
+        <EventSearchBar />
+      </div>
+      <div className="w-full flex flex-col items-center">
+        {events.map((event) => {
+          return <EventCard key={event.id} event={event} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-export default page
+export default page;
