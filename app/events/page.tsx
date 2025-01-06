53d9ac8456b8caae012ad4777ab5b1d604cd9ae3
Risa Yamamoto <yamari2422@gmail.com>
2025-01-05 23:33:55 -0800

    feat: add pagination

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -6,7 +6,7 @@ const page = async () => {
   const events = await getEvents();
 
   return (
-    <section className="flex flex-col items-center gap-5 mt-16 mb-28">
+    <section className="flex flex-col items-center gap-9 mt-16 mb-28">
       <h1 className="text-4xl font-bold font-plus-jakarta-sans tracking-tight">
         Events
       </h1>
